534a35410b98ed2bc27b875d1a31f2b1dc5c72b9
DarkBlade366 <xavierramirezf@gmail.com>
2026-09-22 00:14:51 -0400

    feat: add PostgreSQL/TypeORM and connect to postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST') ?? 'localhost',
+        port: parseInt(configService.get('DATABASE_PORT') ?? '5432', 10),
+        username: configService.get('DATABASE_USER') ?? 'postgres',
+        password: configService.get('DATABASE_PASSWORD') ?? 'postgres',
+        database: configService.get('DATABASE_NAME') ?? 'programacion_web',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
